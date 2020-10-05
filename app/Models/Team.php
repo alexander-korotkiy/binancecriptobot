e50936d3f3d95f44e391e071335367ae4ad1c370
Alex Korotkiy <korotliy.al@gmail.com>
2020-10-05 15:29:47 +0300

    Generate base models doc

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -2,11 +2,38 @@
 
 namespace App\Models;
 
+use Eloquent;
+use Illuminate\Support\Carbon;
 use Laravel\Jetstream\Events\TeamCreated;
 use Laravel\Jetstream\Events\TeamDeleted;
 use Laravel\Jetstream\Events\TeamUpdated;
+use Illuminate\Database\Eloquent\Builder;
 use Laravel\Jetstream\Team as JetstreamTeam;
+use Illuminate\Database\Eloquent\Collection;
 
+/**
+ * App\Models\Team
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string $name
+ * @property bool $personal_team
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read User $owner
+ * @property-read Collection|User[] $users
+ * @property-read int|null $users_count
+ * @method static Builder|Team newModelQuery()
+ * @method static Builder|Team newQuery()
+ * @method static Builder|Team query()
+ * @method static Builder|Team whereCreatedAt($value)
+ * @method static Builder|Team whereId($value)
+ * @method static Builder|Team whereName($value)
+ * @method static Builder|Team wherePersonalTeam($value)
+ * @method static Builder|Team whereUpdatedAt($value)
+ * @method static Builder|Team whereUserId($value)
+ * @mixin Eloquent
+ */
 class Team extends JetstreamTeam
 {
     /**
